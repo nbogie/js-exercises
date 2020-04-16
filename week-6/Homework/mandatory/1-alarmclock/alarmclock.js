1077a8ab1040f2d31c8201dcf51d6a2e8846f1ee
Neill Bogie <neillbogie@googlemail.com>
2020-04-16 23:02:47 +0100

    a sample soln for js2-week3 alarm clock.

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,24 +1,65 @@
-function setAlarm() {}
+//Multiple presses of the "set alarm" button will
+//all reset and set this singleton interval.
+let timerInterval;
+
+//Sets and starts the alarm countdown,
+//based on a value it finds in the DOM
+function setAlarm(audio) {
+  clearTimer();
+  const inputStr = document.getElementById("alarmSet").value;
+  let timeRemaining = Number(inputStr) + 1;
+
+  //A function to call once every second when the timer is running
+  //Note: Both `timeRemaining` and `audio` will be referenced from
+  //the surrounding closure. Alternatively, use global variables,
+  //or pass them to tick()
+  function tick() {
+    timeRemaining -= 1;
+    updateDisplay(timeRemaining);
+    if (timeRemaining <= 0) {
+      playAlarm(audio);
+      clearTimer();
+    }
+  }
+  timerInterval = setInterval(tick, 1000);
+}
+
+//Clear the timer interval, if it has been set.
+//It's ok to call this if interval is not set.
+function clearTimer() {
+  if (timerInterval) {
+    clearInterval(timerInterval);
+    timerInterval = undefined;
+  }
+}
+
+function updateDisplay(secondsRemaining) {
+  const padNum = (n) => n.toString().padStart(2, "0");
+  const mm = padNum(Math.floor(secondsRemaining / 60));
+  const ss = padNum(secondsRemaining % 60);
+
+  const elem = document.getElementById("timeRemaining");
+  elem.textContent = `Time Remaining: ${mm}:${ss}`;
+}
 
 // DO NOT EDIT BELOW HERE
-
-var audio = new Audio("alarmsound.mp3");
-
 function setup() {
+  var audio = new Audio("alarmsound.mp3");
+
   document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
+    setAlarm(audio);
   });
 
   document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
+    stopAlarm(audio);
   });
 }
 
-function playAlarm() {
+function playAlarm(audio) {
   audio.play();
 }
 
-function pauseAlarm() {
+function stopAlarm(audio) {
   audio.pause();
 }
 
